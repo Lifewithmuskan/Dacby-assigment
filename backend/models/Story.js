300abbe3e889121d701e2b2992f4a71ca3510113
Lifewiithmuskan <muskan112002muskan@gmail.com>
2026-05-10 10:02:05 +0530

    schema

---
+++ backend/models/Story.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const StorySchema=new mongoose.Schema({
+    title:String,
+    url:String,
+    points:String,
+    author:String,
+    postedAt:String,
+})
+
+const story=mongoose.model("Story",StorySchema);
+export default Story;
