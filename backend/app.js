b16d5562ec45a29a4d85e26b317a344b743e8f80
Lifewiithmuskan <muskan112002muskan@gmail.com>
2026-05-10 09:34:58 +0530

    MongoDB connected

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv"
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
 
 const app=express();
 app.use(express.json());
@@ -9,7 +14,7 @@ app.get("/",(req,res)=>{
 })
 
 
-const PORT="3000"
+const PORT=process.env.PORT|| "5000";
 app.listen(PORT,()=>{
  console.log(`server running on Port ${PORT}`)
 })
