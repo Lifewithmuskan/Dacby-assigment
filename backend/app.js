da8167e58ce04904c016c00389fafc4315f410b8
Lifewiithmuskan <muskan112002muskan@gmail.com>
2026-05-11 15:15:50 +0530

    signup logic

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js";
 import scrapeStories from "./scraper/scrapeStories.js";
 import Story from "./models/Story.js";
+import bcrypt from "bcryptjs";
+import User from "./models/User.js";
 
 dotenv.config();
 connectDB();
@@ -36,6 +38,48 @@ app.get("/api/allstories", async (req, res) => {
   }
 });
 
+app.post("/api/signup", async (req, res) => {
+
+  try {
+    const {
+      name,
+      email,
+      password
+    } = req.body;
+
+    
+    const existingUser =
+      await User.findOne({ email });
+    if(existingUser){
+      return res.status(400).json({
+        message: "User already exists"
+      });
+
+    }
+
+    const hashedPassword =
+      await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+
+    });
+
+    res.status(201).json({
+      message: "Signup successful",
+      user,
+    });
+
+  } catch(error){
+    console.log(error);
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+});
 const PORT=process.env.PORT|| "5000";
 app.listen(PORT,async()=>{
  console.log(`server running on Port ${PORT}`)
